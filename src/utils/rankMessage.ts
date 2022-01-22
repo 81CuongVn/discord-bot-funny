e8656d7cfd934a3f04dbe772e17a9b33cc7b7335
tritranduc <tritranduc11@gmail.com>
2022-01-22 10:36:44 +0700

    fix rank core

--- src/utils/rankMessage.ts
+++ src/utils/rankMessage.ts
@@ -3,56 +3,56 @@ import { xpUserModel } from "../model/RankUser";
 import { IClient } from "./../types/index";
 export default async function xpMessage(message: Message, client: IClient) {
   let xp = 0;
-  if (message.content.length <= 1024) {
-    xp = xp + 50;
-  } else {
+  if (message.content.length >= 1024) {
     xp = xp + 100;
   }
-  if (message.content.length <= 500) {
+  if (message.content.length >= 500) {
     xp = xp + 25;
   }
-  if (message.content.length <= 100) {
+  if (message.content.length >= 100) {
     xp = xp + 10;
   }
-  if (message.content.length <= 50) {
+  if (message.content.length >= 50) {
     xp = xp + 5;
   }
-  if (message.content.length <= 20) {
+  if (message.content.length >= 20) {
     xp = xp + 2;
   }
-  if (message.content.length <= 10) {
+  if (message.content.length >= 10) {
     xp = xp + 1;
   }
-  if (message.content.length <= 5) {
+  if (message.content.length >= 5) {
     xp = xp + 0.5;
   }
-  if (message.content.length <= 3) {
+  if (message.content.length >= 3) {
     xp = xp + 0.25;
   }
-  if (message.content.length <= 2) {
+  if (message.content.length >= 2) {
     xp = xp + 0.1;
   }
-  if (message.content.length <= 1) {
+  if (message.content.length >= 1) {
     xp = xp + 0.05;
   }
-  if (message.content.length <= 0) {
+  if (message.content.length >= 0) {
     xp = xp + 0.01;
   }
   const addPoint = Math.round(Math.random() * 100) + 1;
   const xpFinal = xp + addPoint;
+  const serverId = message.guild?.id;
   const lastXpInDatabase = await xpUserModel.findOne({
     userId: message.author.id,
-    serverId: message.guild?.id,
+    serverId: serverId,
   });
-  const serverId = message.guild?.id;
+
   if (lastXpInDatabase) {
+    console.log(lastXpInDatabase?.xpMessage + xpFinal);
     await xpUserModel.findOneAndUpdate(
       {
         _id: lastXpInDatabase._id,
         serverId: serverId,
       },
       {
-        xp: lastXpInDatabase.xpMessage + xpFinal,
+        xpMessage: lastXpInDatabase.xpMessage + xpFinal,
       }
     );
   } else {
@@ -66,5 +66,6 @@ export default async function xpMessage(message: Message, client: IClient) {
   }
   return await xpUserModel.findOne({
     userId: message.author.id,
+    serverId: serverId,
   });
 }
