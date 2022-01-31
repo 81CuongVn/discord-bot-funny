4710d763418095495337e6162bcfc2f29cca839f
tritranduc <tritranduc11@gmail.com>
2022-01-31 11:36:33 +0700

    :)

--- src/handlers/MessageCommand.ts
+++ src/handlers/MessageCommand.ts
@@ -3,31 +3,43 @@ import path from "path";
 import { table } from "table";
 import { IClient } from "./../types/index";
 var dataLog = [["filename", "status"]];
+import glob from "glob";
+import { promisify } from "util";
 
-const MessageCommandHandler = (client: IClient) => {
-  var dirMain = readdirSync(path.join(__dirname, "../commands/"));
-  dirMain.forEach((dir) => {
-    var commands = readdirSync(
-      path.join(__dirname, `../commands/${dir}/`)
-    ).filter((file) => file.endsWith(".ts"));
-    for (const file of commands) {
-      const pathToFile = path.join(__dirname, `../commands/${dir}/${file}`);
-      var pull = require(pathToFile).default;
-      if (pull?.name) {
-        client.commands?.set(pull.name, pull);
-        dataLog.push([file, "☑ file status is good"]);
-      } else {
-        dataLog.push([file, " thieu name"]);
-        continue;
-      }
-      if (pull.aliases && Array.isArray(pull.aliases)) {
-        pull.aliases.map((alias: string) => {
-          client.aliases?.set(alias, pull.name);
-        });
-      }
-    }
+const globPromise = promisify(glob);
+
+async function ImportFile(filePath: string) {
+  const file = (await import(filePath)).default;
+  return file;
+}
+
+const MessageCommandHandler = async (client: IClient) => {
+  // var dirMain = readdirSync(path.join(__dirname, "../commands/"));
+  // dirMain.forEach((dir) => {
+  //   var commands = readdirSync(
+  //     path.join(__dirname, `../commands/${dir}/`)
+  //   ).filter((file) => file.endsWith(".ts"));
+
+  // });
+  const commandFile = await globPromise(`../commands/*/*{.ts,.js}`, {
+    cwd: __dirname,
   });
-
+  for (const file of commandFile) {
+    const fileName = path.join(__dirname, file);
+    const pull = await ImportFile(fileName);
+    if (pull?.name) {
+      client.commands?.set(pull.name, pull);
+      dataLog.push([file, "☑ file status is good"]);
+    } else {
+      dataLog.push([file, " thieu name"]);
+      continue;
+    }
+    if (pull.aliases && Array.isArray(pull.aliases)) {
+      pull.aliases.map((alias: string) => {
+        client.aliases?.set(alias, pull.name);
+      });
+    }
+  }
   console.log("message command handlers đã được load xong");
   console.log(table(dataLog));
 };
